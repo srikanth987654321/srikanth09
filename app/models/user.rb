8784d4ff1e5c0d33e31c6f1156988d45f25fc1e1
KolaRavali <113207723+srikanth987654321@users.noreply.github.com>
2023-01-07 14:01:39 +0530

    hii

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :email, presence: true
-
-  has_secure_password
-
-  has_many :todos
-end
--- db/migrate/20220328154452_improve_users.rb
+++
@@ -1,9 +0,0 @@
-class ImproveUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :name, :string
-    remove_column :users, :password, :string
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :password_digest, :string
-  end
-end
